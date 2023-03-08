1c15a3212432e4f98c8074a28a49a1c629828739
Erdoo Kuhwa <erdoo.kuhwa@gmail.com>
2023-03-08 17:44:40 +0000

    + cloudwatch logs & watchtower

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+import logging
 
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
+    # logger.info("bootcamp home activities logs")
     with tracer.start_as_current_span("home-activities-mock-data"):
       # Honeycomb attribute -----------------
       span = trace.get_current_span()
