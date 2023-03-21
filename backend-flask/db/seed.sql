211fa470ba03abcaf5260ae2ccf7ca95df5ca361
Erdoo Kuhwa <erdoo.kuhwa@gmail.com>
2023-03-21 03:07:35 +0000

    + fixed creating activities

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown', 'ab@domain.com', 'MOCK'),
+  ('Everlygrandest', 'everlygrandest', 'everlygrandest@gmail.com', 'MOCK'),
   ('Andrew Bayko', 'bayko', 'bayko@domain.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'everlygrandest' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
