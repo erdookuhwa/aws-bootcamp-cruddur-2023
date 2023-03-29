b60b7d049f0706c38cbeca6d7acadfc2bb0d7f81
Erdoo Kuhwa <erdoo.kuhwa@gmail.com>
2023-03-29 17:05:23 +0000

    + fix list messages

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -15,36 +15,36 @@ class Ddb:
     ddb = boto3.client('dynamodb',**attrs)
     return ddb  
 
-def list_message_groups(client, my_user_uuid):
-    current_year = datetime.now().year
-    table_name = 'cruddur-messages'
-    query_params = {
-    'TableName': table_name,
-    'KeyConditionExpression': 'pk = :pk AND sk BETWEEN :start_date AND :end_date',
-    'ScanIndexForward': False,
-    'Limit': 20,
-    'ExpressionAttributeValues': {
-      ':year': {'S': str(current_year) },
-      ':pk': {'S': f"GRP#{my_user_uuid}"}
+  def list_message_groups(client, my_user_uuid):
+      current_year = datetime.now().year
+      table_name = 'cruddur-messages'
+      query_params = {
+      'TableName': table_name,
+      'KeyConditionExpression': 'pk = :pk AND begins_with(sk, :year)',
+      'ScanIndexForward': False,
+      'Limit': 20,
+      'ExpressionAttributeValues': {
+        ':year': {'S': str(current_year) },
+        ':pk': {'S': f"GRP#{my_user_uuid}"}
+        }
       }
-    }
-    print('query-params')
-    print(query_params)
-    print('client')
-    print(client)
+      print('query-params')
+      print(query_params)
+      print('client')
+      print(client)
 
-    # query the table
-    response = client.query(**query_params)
-    items = response['Items']
+      # query the table
+      response = client.query(**query_params)
+      items = response['Items']
 
-    results = []
-    for item in items:
-        last_sent_at = item['sk']['S']
-        results.append({
-            'uuid': item['message_group_uuid']['S'],
-            'display_name': item['user_display_name']['S'],
-            'handle': item['user_handle']['S'],
-            'message': item['message']['S'],
-            'created_at': last_sent_at
-    })
-    return results
+      results = []
+      for item in items:
+          last_sent_at = item['sk']['S']
+          results.append({
+              'uuid': item['message_group_uuid']['S'],
+              'display_name': item['user_display_name']['S'],
+              'handle': item['user_handle']['S'],
+              'message': item['message']['S'],
+              'created_at': last_sent_at
+      })
+      return results
