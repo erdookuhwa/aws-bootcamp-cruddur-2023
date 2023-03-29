404300040f3fbf31a4f2b8b5e2e9204144703afb
Erdoo Kuhwa <erdoo.kuhwa@gmail.com>
2023-03-29 21:38:48 +0000

    + begin create msg new

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,8 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Everly Grandest', 'everlygrandest', 'everlygrandest@gmail.com', 'MOCK'),
   ('Andrew Brown', 'andrewbrown', 'andrewbrown@exampro.co', 'MOCK'),
-  ('Andrew Bayko', 'bayko', 'bayko@domain.com', 'MOCK');
+  ('Andrew Bayko', 'bayko', 'bayko@domain.com', 'MOCK'),
+  ('Jane Doe', 'janedoe', 'janedoe@yahoo.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
