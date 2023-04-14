d0a1b3c690bbafcf40a6e1e5f3b550ea2860d817
Erdoo Kuhwa <erdoo.kuhwa@gmail.com>
2023-04-14 17:55:44 +0000

    + complete xray insights, troubleshoot messages

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -7,7 +7,7 @@ export async function getAccessToken(){
     const access_token = cognito_user_session.accessToken.jwtToken
     localStorage.setItem("access_token", access_token)
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 }
 
 export async function checkAuth(setUser){
