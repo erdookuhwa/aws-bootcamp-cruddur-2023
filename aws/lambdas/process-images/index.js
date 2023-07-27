7a5a19fb7432fca6cdcbbc5a7468de2e42d76c7c
Erdoo Kuhwa <erdoo.kuhwa@gmail.com>
2023-07-27 15:20:33 +0000

    + tests

--- aws/lambdas/process-images/index.js
+++ aws/lambdas/process-images/index.js
@@ -3,7 +3,7 @@ const {getClient, getOriginalImage, processImage, uploadProcessedImage} = requir
 const path = require('path');
 
 const bucketName = process.env.DEST_BUCKET_NAME
-// const folderInput = process.env.FOLDER_INPUT
+const folderInput = process.env.FOLDER_INPUT
 const folderOutput = process.env.FOLDER_OUTPUT
 const width = parseInt(process.env.PROCESS_WIDTH)
 const height = parseInt(process.env.PROCESS_HEIGHT)
